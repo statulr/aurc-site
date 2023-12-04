e735be853889b516a95b9d80ed5836adf8dadd7d
Hamza <72913359+Hamza12700@users.noreply.github.com>
2023-12-04 10:37:12 -0700

    added `Geist Mono Meduim`

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,7 +4,8 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        gesitMedium: ["Geist Medium"],
+        gesitMonoMedium: ["Geist Mono Medium"],
+        geistRegSemiBold: ["Gesit Regular SemiBold"],
       },
     },
   },
